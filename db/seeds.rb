6d30f1e50d9051fe0bab557b372872e95c5b65b5
jaxcode1204 <jaxcode1204>
2025-09-17 19:18:44 -0500

    Seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create!([
+    {title: "Harry Potter and the Philosophers Stone"},
+    {title: "The Shining"},
+    {title: "Diary of a Wimpy Kid"},
+    {title: "Band of Brothers"},
+    {title: "Boys of Winter"}
+])
